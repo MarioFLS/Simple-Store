6b70a509094a5996fbc8ef112e2ca89a6c659865
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-10 15:32:02 -0300

    feat: Entidade Departament

---
+++ ProjectStoreDotNet/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectStoreDotNet.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
