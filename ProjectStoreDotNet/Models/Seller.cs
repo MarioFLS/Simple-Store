66d5a95546feb182ec47a5cbe03e104ad84b3ef2
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-10 15:32:14 -0300

    feat: Entidade Seller

---
+++ ProjectStoreDotNet/Models/Seller.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectStoreDotNet.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Column("birth_date")]
+        [DisplayFormat(DataFormatString = "{dd/MM/yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [Column("base_salary", TypeName = "decimal(6,2)")]
+        public decimal BaseSalary { get; set; }
+
+        public Department Department { get; set; }
+        [ForeignKey("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        public Seller(int id, string name, string email, DateTime birthDate, decimal baseSalary, int departmentId)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            DepartmentId = departmentId;
+        }
+    }
+}
