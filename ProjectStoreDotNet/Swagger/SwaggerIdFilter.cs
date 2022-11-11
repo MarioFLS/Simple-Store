3ca4042a36f134ae36a3a8b2ad30c6f81d46915a
MarioFLS <mariofernandolsantos@gmail.com>
2022-11-10 23:02:39 -0300

    feat: filtro do swagger

---
+++ ProjectStoreDotNet/Swagger/SwaggerIdFilter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ProjectStoreDotNet.Swagger
+{
+    public class SwaggerIdFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            schema.Properties.Remove("id");
+        }
+    }
+}
